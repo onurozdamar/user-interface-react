9dd98e46c44bd447f24b8fdd65832304bc09e0b3
Onur Özdamar <63419558+onurozdamar@users.noreply.github.com>
2021-07-24 21:28:33 +0300

    add buttons and style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import MyCard from "./MyCard";
-import React, { useEffect } from "react";
+import MyCard from "./components/MyCard";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers as getUsersAction } from "./store/actions";
+import { Button } from "@material-ui/core";
+import MyCardModal from "./components/MyCardModal";
 
-// TODO: asas
 // TODO: add ve update modal
 // TODO: delete button
 // TODO: filtre modal
 // TODO: card liste
+
+const styles = {
+  container: {
+    width: "75%",
+    margin: "0 auto",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: "10px",
+    justifyContent: "space-around",
+  },
+};
 
 function App() {
   const dispatch = useDispatch();
@@ -22,11 +35,37 @@ function App() {
     getUsers();
   }, [users.length]);
 
+  const [showModal, setShowModal] = useState(false);
+
+  function openModal() {
+    setShowModal(true);
+  }
+
+  function closeModal() {
+    setShowModal(false);
+  }
+
   return (
     <div className="App" style={{ margin: "10px" }}>
-      {users.map((e) => (
-        <MyCard key={e.id} user={e}></MyCard>
-      ))}
+      <div style={styles.container}>
+        <Button variant="contained" color="primary" onClick={openModal}>
+          Card Ekle
+        </Button>
+        <Button variant="contained" color="primary">
+          Sırala
+        </Button>
+        <Button variant="contained" color="primary">
+          Filtrele
+        </Button>
+      </div>
+
+      <div style={styles.container}>
+        {users.map((user) => (
+          <MyCard key={user.id} user={user}></MyCard>
+        ))}
+      </div>
+
+      <MyCardModal showModal={showModal} closeModal={closeModal} />
     </div>
   );
 }
